03a23fb598ddd23a20b50744f6296c48c6a58c49
Guolin.Wan <wanguolin@gmail.com>
2014-01-15 22:42:08 +0800

    finished 2 functions - random_gen and chk_sorting

--- ChapterII__Sorting_and_Order_Statictics/gen_random.c
+++ ChapterII__Sorting_and_Order_Statictics/gen_random.c
@@ -5,24 +5,39 @@
 
 #define ARR_SIZE_RANGE    50
 #define ARR_SIZE_MIN      15
+#define NUM_IN_ARR_RANGE  100
 
-int gen_array(int* ptr_arr[], int* arr_size);
+int gen_array(int **ptr_arr, int *arr_size);
+bool chk_sorting( bool is_ascending, int **ptr_arr, const int arr_size);
 
-int gen_array(int* ptr_arr[], int* arr_size)
+int gen_array(int **ptr_arr, int *arr_size)
 {
     srand( (unsigned)time( NULL));
-    int rand_arr_size = rand()%ARR_SIZE_RANGE + ARR_SIZE_MIN;
-    *ptr_arr = new int[rand_arr_size];
-    printf("arr size:%d\n", rand_arr_size);
-
-
+    *arr_size = rand()%ARR_SIZE_RANGE + ARR_SIZE_MIN;
+    *ptr_arr  = new int[*arr_size];
+    for( int i = 0; i < *arr_size; ++i)
+        (*ptr_arr)[i] = rand()%NUM_IN_ARR_RANGE;        
 }
 
+bool chk_sorting( bool is_ascending, int **ptr_arr, const int arr_size)
+{
+    for( int i = 0; i < arr_size - 1; ++i)
+        for( int j = i; j < arr_size; ++j)
+            if( !( is_ascending && ptr_arr[i] <= ptr_arr[j]) || !( !is_ascending && ptr_arr[i] >= ptr_arr[j]))
+                return false;
+    return true;
+}
 
 int main()
 {
-    int size;
-    int arr;
-    gen_array( (int**)*)&arr, &size);
+    int size = 0, *arr = NULL;
+    gen_array( &arr, &size);
+
+    for( int i = 0; i < size; ++i)
+        printf("%d ", arr[i]);
+    
+    chk_sorting( true, &arr, size) == true ? printf( "true\n") : printf( "false\n");
+
+    return 0;
 
 }
